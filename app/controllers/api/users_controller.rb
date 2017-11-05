225be2d3720931afacd06609f9a716dabb0b6ae8
Arthur Philadelpho <arthur22rj@gmail.com>
2017-11-04 22:07:27 -0200

    Created 'create' and 'destroy' actions.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,9 +7,29 @@ class Api::UsersController < ApiController
   	render json: users, each_serializer: UserSerializer
   end
 
+  def create
+  	user = User.new(user_params)
+  	if user.save
+  		render json: user
+  	else
+  		render json: {errors: user.errors.full_messages},
+  		status :unprocessable_entity
+  	end
+  end
+
+  def destroy
+  	begin 
+  		user = User.find(params[:id])
+  		user.destroy
+  		render json: {}, status: :no_content
+  	rescue
+  		render :json => {}, :status => :not_found
+  	end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :password_digest)
   end
 end
